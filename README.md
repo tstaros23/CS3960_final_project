0ab3e1f44e7a94f1d4aa9669c08becb28f16af4d
theodorestaros <tstaros23@gmail.com>
2026-04-21 18:50:44 -0600

    Updated the README and added two more constraints

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CS3960_final_project
+
+## How to run Streamlit:
+### If Streamlit is not installed, in your terminal install with pip `pip install streamlit`
+### Then from your terminal, run the app `streamlit run conformance_constraints_demo.py`
+###Streamlit should open automatically in the webbrowser. It is hosted on http://localhost:8501/
--- conformance_constraints_demo.py
+++ conformance_constraints_demo.py
@@ -10,17 +10,41 @@ if uploaded_file:
     st.write("Preview of data:")
     st.dataframe(df.head())
 
-column = st.selectbox("Select column for range check", df.columns)
-min_val = st.number_input("Min value")
-max_val = st.number_input("Max value")
+    column = st.selectbox("Select column for year formatting check", df.columns)
 
-if st.button("Check Range"):
-    #pandas uses vectors to perform operations, so I did not loop manually and applied conditions to the entire column. 
-    #so we are comparing two series and creating a new series of validations
-    violations = df[(df[column] < min_val) | (df[column] > max_val)]
-    
-    if violations.empty:
-        st.success("No violations")
-    else:
-        st.error(f"{len(violations)} violations found")
-        st.dataframe(violations)
+    if st.button("Check Year Format (YYYY-MM-DD)"):
+        #pandas uses vectors to perform operations, so I did not loop manually and applied conditions to the entire column. 
+        #so we are comparing series and creating a new series of validations
+        df["parsed_date"] = pd.to_datetime(df[column], format="%Y-%m-%d", errors="coerce")
+
+        violations = df[df["parsed_date"].isna()]
+
+        if violations.empty:
+            st.success("No violations")
+        else:
+            st.error(f"{len(violations)} violations found")
+            st.dataframe(violations)
+            percent = (len(violations) / len(df)) * 100
+            st.write(f"{round(percent, 2)}% of the data is invalid")
+
+    if st.button("Check For Missing Values"):
+        violations = df[df[column].isna()]
+
+        if violations.empty:
+            st.success("No violations")
+        else:
+            st.error(f"{len(violations)} violations found")
+            st.dataframe(violations)
+            percent = (len(violations) / len(df)) * 100
+            st.write(f"{round(percent, 2)}% of the data is missing")
+    if st.button("Check For Duplicate Values"):
+        #returns false in the violations series if the value is not duplicated
+        violations = df[df[column].duplicated()]
+
+        if violations.empty:
+            st.success("No violations")
+        else:
+            st.error(f"{len(violations)} violations found")
+            st.dataframe(violations)
+            percent = (len(violations) / len(df)) * 100
+            st.write(f"{round(percent, 2)}% of the data is duplicated")
